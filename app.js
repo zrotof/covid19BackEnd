4acc17e3035bbe888b7a63b4301aa0ee91800b90
Zrotof <manduel21@gmail.com>
2020-08-06 04:17:23 +0200

    trying to fix pb on deploy 6, changing the procfile

--- app.js
+++ app.js
@@ -66,4 +66,4 @@ app.use(cors());
 
 //How tolisten to the server
 const port = process.env.PORT || 3000
-app.listen(port, '0.0.0.0', ()=>console.log(`Listening on port : ${port} ...`));
+app.listen(port, '0.0.0.0', ()=>console.log(`Listening on port : ${port}...`));

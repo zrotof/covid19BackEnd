ee94fcc3707e177586f37f1d88e2453ed0a45351
Zrotof <manduel21@gmail.com>
2020-08-06 18:24:23 +0200

    fix DATABASE_URL problem 3

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 const express = require('express');
 const Country = require("./models/country");
 const Continant = require("./models/continent");

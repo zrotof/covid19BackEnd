eb350a552528a5dcb999fd1dd96c35d43b1bec3a
Zrotof <manduel21@gmail.com>
2020-08-07 04:18:22 +0200

    fix DATABASE_URL problem 25

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ console.log("url : >>>>", process.env.DATABASE_URL);
 module.exports = {
 
     "type": "postgres",
-    "url": process.env.DATABASE_URL
+    "url": "postgres://postgres:LeKWASSAkwassa001@localhost:5432/covid19"
 
 }
